37233cea32b4401b9a79e3ae1473401f60dd1404
Severino Mateus Jr <severmateus@localhost.localdomain>
2018-01-11 18:36:49 +0100

    Added Local Database

--- src/main/java/com/domain/resources/DataAccess.java
+++ src/main/java/com/domain/resources/DataAccess.java
@@ -2,15 +2,48 @@ package com.domain.resources;
 
 public class DataAccess {
 	public static String getPrice(String id) {
-		String price = "The price for item "+ id + " is 123.33";
+		String price;		
+		Packet p = getPacket(Integer.parseInt(id));
+		
+		if (!String.valueOf(p.getPrice()).isEmpty()) {
+			price = "The price for item "+ id + " is "+p.getPrice()+" Euros";
+		}
+		else {
+			price = "Item not found. Please verify the item ID and try again";
+		}
 		return price;
 	}
 	
 	public static String getStatus(String id) {
-		String status = "Order "+ id+ " is in Madrid, and will be arriving Tomorrow";
+		String status;		
+		Packet p = getPacket(Integer.parseInt(id));
 		
+		if (!String.valueOf(p.getLocation()).isEmpty()) {
+			status = "Order "+ id+ " is in "+p.getLocation()+", and will be arriving "+p.getETA();
+		}
+		else {
+			status = "Order not found. Please verify the order ID and try again";
+		}
 		return status;
-		
+	}
+	
+	private static Packet getPacket(int id) {
+		if (id == 1)
+		return new Packet (1, "Madrid", (float) 223.45, "Tomorrow");
+		else
+			if (id == 2)
+				return new Packet (2, "Barcelona", (float) 223.45, "in 2 days");
+			else
+				if (id == 3)
+					return new Packet (3, "Paris", (float) 223.45, "in 5 days");
+				else
+					if (id == 4)
+						return new Packet (4, "Lisbon", (float) 223.45, "on Sunday");
+					else
+						if (id == 5)
+							return new Packet (5, "London", (float) 223.45,  "not so soon");
+						else
+							return new Packet();	
 	}
 
 }
---
+++ src/main/java/com/domain/resources/Packet.java
@@ -0,0 +1,50 @@
+package com.domain.resources;
+
+public class Packet {
+	int id;
+	String location, ETA;
+	float price;
+	
+	public Packet(int id, String location, float price, String ETA) {
+		this.id = id;
+		this.location = location;
+		this.price = price;
+		this.ETA = ETA;
+	}
+	
+	public Packet() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getETA() {
+		return ETA;
+	}
+
+	public void setETA(String eTA) {
+		ETA = eTA;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
